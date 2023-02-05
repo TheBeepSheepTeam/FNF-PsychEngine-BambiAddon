34872c36a228ee801ab83c27c6c169a29899c91f
CharlesCatYT <62410083+ShadowMilky@users.noreply.github.com>
2023-02-05 17:16:44 -0500

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -145,7 +145,7 @@ class MainMenuState extends MusicBeatState
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Bambi Addon v" + bambiAddonVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Foxa Addon v" + bambiAddonVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
