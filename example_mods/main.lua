6b4eea659fb9fd4294f6bf64803fd4f08a07ac86
CharlesCatYT <62410083+ShadowMilky@users.noreply.github.com>
2023-02-05 17:18:58 -0500

    Update main.lua

--- example_mods/main.lua
+++ example_mods/main.lua
@@ -1,16 +1,16 @@
 --settings and stuff DONT DELETE ANYTHING!!!!!!!
 function getMainStuff()
   local data = {
-    windowTitle = 'Psych Engine: Bambi Addon!', --title of the window
+    windowTitle = 'Psych Engine: Foxa Addon!', --title of the window
     iconsPath = 'icons/', --path of the app icons
     modName = 'Your Mod Name That You Can Change In "mods/main.lua"',
     modVersion = '0.0.1',
     iconBop = true, --set to false for custom icon bops,
-    saveFile = 'PsychEngineBambiAddon', --name of the save file for your mod
+    saveFile = 'PsychEngineFoxaAddon', --name of the save file for your mod
     flashingStateText = '', --this gets added on to the normal flashing state text, use it if you're mod has like super flashing lights or something
     initialState = nil, --set this to skip to a custom state on startup
-    updateTxtLink = 'https://raw.githubusercontent.com/mayo78/FNF-PsychEngine-BambiAddon/main/gitVersion.txt', --this is a link to an online file with the current mod's vesrion on it
-    updateLink = 'https://github.com/mayo78/FNF-PsychEngine-BambiAddon/releases', --where to send the user for the latest release
+    updateTxtLink = 'https://raw.githubusercontent.com/TheBeepSheepTeam/FNF-PsychEngine-FoxaAddon/main/gitVersion.txt', --this is a link to an online file with the current mod's vesrion on it
+    updateLink = 'https://github.com/TheBeepSheepTeam/FNF-PsychEngine-FoxaAddon/releases', --where to send the user for the latest release
     outdatedText = [[
     Sup bro, looks like you're running an
     outdated version of Psych Engine (_VERSION),
